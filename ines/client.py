8fb36a9326284c4c60677e4a42043b6d422db302
ines-kr <ineskrichene87@gmail.com>
2022-04-27 14:02:35 +0100

    threads creation

--- ines/client.py
+++ ines/client.py
@@ -1,13 +1,13 @@
 import socket
 
-hote = "localhost"
-port = 15555
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("", 1111))
 
-socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-socket.connect((hote, port))
-print ("Connection on {}".format(port))
-
-socket.send(b"Hey my name is Olivier!")
-
-print ("Close")
-socket.close()
+print("Le nom du fichier que vous voulez récupérer:")
+file_name = input(">> ") # utilisez raw_input() pour les anciennes versions python
+s.send(file_name.encode())
+file_name = 'data/%s' % (file_name,)
+r = s.recv(9999999)
+with open(file_name,'wb') as _file:
+    _file.write(r)
+print("Le fichier a été correctement copié dans : %s." % file_name)
---
+++ ines/main_banque.py
@@ -0,0 +1,29 @@
+def affichage_banque():
+    print("Choisissez l'operation a executer")
+    print("1 : Consulter la liste des comptes")
+    print("2 : Consulter la facture d’un compte")
+    print("3 : Consulter l’historique des transactions")
+    x=int(input("Votre choix : "))
+    return(x)
+
+def demande_ref():
+    ref=input("donnez la reference du compte a consulter")
+    return(ref)
+
+def demande_Consulter_liste_comptes(ref):
+
+def demande_Consulter_facture_compte(ref):
+
+def demande_Consulter_historique_transactions(ref):
+
+def main_banque():
+    choix=0
+    choix=affichage_banque()
+    if ( choix==1 ):
+        ref=demande_ref()
+        
+    elif ( choix==2 ):
+    elif ( choix==3 ):
+    else:
+
+
